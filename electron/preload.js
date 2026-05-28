a0dd0307792f4aeb1da2adffe6faa885ac4dce97
sunpcn <sunpcn@gmail.com>
2026-05-28 23:10:37 +0800

    desktop: expose update IPC in preload

--- electron/preload.js
+++ electron/preload.js
@@ -5,4 +5,11 @@ contextBridge.exposeInMainWorld('electronAPI', {
   selectDirectory: () => ipcRenderer.invoke('select-directory'),
   openPath: (p) => ipcRenderer.invoke('open-path', p),
   getVersion: () => ipcRenderer.invoke('app-version'),
+  checkForUpdates: () => ipcRenderer.invoke('update-check'),
+  installUpdate: () => ipcRenderer.invoke('update-install'),
+  onUpdateStatus: (cb) => {
+    const handler = (_e, payload) => cb(payload);
+    ipcRenderer.on('update-status', handler);
+    return () => ipcRenderer.removeListener('update-status', handler);
+  },
 });
